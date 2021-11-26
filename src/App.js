8e26eb80ed682ead5b1459f7642ea041b1f7ff67
Harman Singh <hsingh@sailthru.com>
2021-11-27 00:50:18 +1300

    Get started with material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import {QRScanner} from "./components/QRScanner";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
-  return (
-    <div className="App">
-        <QRScanner/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Box sx={{flexGrow: 1}}>
+                    <QRScanner/>
+                </Box>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import {ThemeProvider} from '@mui/material/styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline enableColorScheme/>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
