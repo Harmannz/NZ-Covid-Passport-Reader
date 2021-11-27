63e73c897e9978eed10cfb7178dc09d0b368c382
Harman Singh <hsingh@sailthru.com>
2021-11-27 17:22:19 +1300

    Add route so that app can render on client side

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import {QRScanner} from "./components/QRScanner";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Container maxWidth="md" >
-                <Box sx={{my: 4, display: 'flex', flexDirection: 'column'}}>
-                    <Typography variant="h4" component="h1" gutterBottom >
-                        NZ Covid Passport Reader
-                    </Typography>
-                    <div style={{flexGrow: 2}}>
-                        <QRScanner/>
-                    </div>
-                </Box>
-            </Container>
-        </div>
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="App">
+                            <Container maxWidth="md">
+                                <Box sx={{my: 4, display: 'flex', flexDirection: 'column'}}>
+                                    <Typography variant="h4" component="h1" gutterBottom>
+                                        NZ Covid Passport Reader
+                                    </Typography>
+                                    <div style={{flexGrow: 2}}>
+                                        <QRScanner/>
+                                    </div>
+                                </Box>
+                            </Container>
+                        </div>
+                    }
+                />
+            </Routes>
+        </Router>
     );
 }
 
